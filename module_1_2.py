b75b064b74efeee49fac5cc829ee08dfa7c788b4
livintin <natig.abdulov@gmail.com>
2024-07-31 19:37:49 +0700

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,6 @@
+completed_homework = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = hours_spent/completed_homework
+print("Курс:" + course_name + ", всего задач:" + str(completed_homework) + ", затрачено часов:" + str(hours_spent) + ", среднее время выполнения:" + str(time_for_one_task) + " часа")
+ 
